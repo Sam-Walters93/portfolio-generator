79d20a9816b7eeaffc0c2d9c5753e7758a3b246c
Sam Walters <stwalters314@gmail.com>
2021-04-05 20:46:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
-  profileDataArr.forEach((index) => {
-    console.log(index);
-  });
+  profileDataArr.forEach(index => console.log(index));
 };
 
 printProfileData(profileDataArgs);
